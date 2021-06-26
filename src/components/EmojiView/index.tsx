236f1054c7764c77c52d25573a79a8c9bbebf82b
alexsandrrh <sascha.sadov@yandex.ru>
2021-06-26 23:37:37 +0300

    add arrow down to on view

--- src/components/EmojiView/index.tsx
+++ src/components/EmojiView/index.tsx
@@ -158,29 +158,74 @@ class EmojiView extends Component<ViewProps, ViewState> {
         ref={refView}
         className={isOpen ? styles.EmojiViewActive : styles.EmojiView}
       >
-        <div className={styles.EmojiViewBody}>{current?.view}</div>
-        <div className={styles.EmojiViewTabs}>
-          {tabsViews.map((item) => (
-            <EmojiViewTab
-              key={item.name}
-              isActive={item.name === this.state.currentTab}
-              onClick={this.handleTabClick(item.name)}
-              icon={item.icon}
-            />
-          ))}
+        <div className={styles.EmojiViewBody}>
+          <div className={styles.EmojiViewItems}>{current?.view}</div>
+          <div className={styles.EmojiViewTabs}>
+            {tabsViews.map((item) => (
+              <EmojiViewTab
+                key={item.name}
+                isActive={item.name === this.state.currentTab}
+                onClick={this.handleTabClick(item.name)}
+                icon={item.icon}
+              />
+            ))}
+          </div>
         </div>
+
         <svg
-          className={styles.EmojiViewСorner}
+          width="22"
+          height="13"
+          viewBox="0 0 22 13"
+          fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="5"
-          viewBox="0 0 16 5"
+          className={styles.EmojiViewСorner}
         >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M8 5C5.19927 5 3.19955 0 0 0H16C12.7982 0 10.8007 5 8 5Z"
-          />
+          <g filter="url(#filter0_d)">
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M11 8C8.19927 8 6.19955 3 3 3H11H19C15.7982 3 13.8007 8 11 8Z"
+              fill="#EBEEF2"
+            />
+            <path
+              d="M19 3.5V3V2.5H11H3V3V3.5C3.66322 3.5 4.28976 3.75772 4.92251 4.19315C5.56039 4.6321 6.16731 5.22484 6.796 5.85355C6.8395 5.89705 6.88314 5.94077 6.92692 5.98462C7.50164 6.56029 8.09989 7.15953 8.72031 7.62019C9.39111 8.11825 10.1461 8.5 11 8.5C11.854 8.5 12.6088 8.11824 13.2794 7.62015C13.8995 7.15965 14.4973 6.56065 15.0717 5.98518C15.1157 5.94113 15.1595 5.89721 15.2032 5.85351C15.8317 5.22482 16.4386 4.6321 17.0765 4.19317C17.7093 3.75777 18.3361 3.5 19 3.5Z"
+              stroke="#C5D0DB"
+            />
+          </g>
+          <rect x="2" y="1" width="18" height="2" fill="#EBEEF2" />
+          <defs>
+            <filter
+              id="filter0_d"
+              x="0"
+              y="0"
+              width="22"
+              height="13"
+            >
+              <feFlood flood-opacity="0" result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              />
+              <feOffset dy="1" />
+              <feGaussianBlur stdDeviation="1.5" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.1 0"
+              />
+              <feBlend
+                mode="normal"
+                in2="BackgroundImageFix"
+                result="effect1_dropShadow"
+              />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow"
+                result="shape"
+              />
+            </filter>
+          </defs>
         </svg>
       </div>
     );
