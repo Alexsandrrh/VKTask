d2cea7ef852855dd08048558d7d683d00e680b26
alexsandrrh <sascha.sadov@yandex.ru>
2021-06-26 22:15:56 +0300

    update styles & init pretty editor

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -10,20 +10,48 @@ import IconSmile from "../../assets/icons/icon-smile.svg";
 import Icon from "../Commons/Icon";
 import EmojiView from "../EmojiView";
 
+const test =
+  "Привет, @tc!\n" +
+  "Выложил тестовое в репозиторий\n" +
+  "https://github.com/CoolCoder/task 🔥🔥🔥\n" +
+  "Было непросто, но весело. Если что, пиши\n" +
+  "на почту coolcoder@ya.ru #nofilters";
+
 const Editor = () => {
   const [isOpenEmoji, setToggleEmoji] = useState(false);
   const input: RefObject<HTMLDivElement> = useRef(null);
   const view: RefObject<HTMLDivElement> = useRef(null);
   const buttonEmoji: RefObject<HTMLDivElement> = useRef(null);
+  let timer: any = null;
+  const pretty = (value: string) => {
+    const PATTERNS = {
+      URL: /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi,
+      HASHTAG: /#(\w+)/g,
+      EMAIL:
+        /(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/g,
+    };
+    if (timer) {
+      clearTimeout(timer);
+      timer = null;
+    }
+    timer = setTimeout(() => {
+      // Set pretty value
+      if (input.current) {
+        input.current.innerHTML = value;
+      }
+    }, 1000);
+  };
 
   const handleChange = (e: ChangeEvent<HTMLDivElement>) => {
-    console.log(e.target.innerHTML);
+    let value = e.target.innerHTML;
+    pretty(value);
   };
 
   const handleSelect = (payload: { type: string; value: string }) => {
     if (input.current) {
       if (payload.type === "emoji") {
         input.current.innerHTML += `<img data-type="emoji" alt="Эмодзи" src="${payload.value}">`;
+        pretty(input.current.innerHTML);
       }
     }
   };
